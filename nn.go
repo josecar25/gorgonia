9418d6f9fc25a34d5b98c926910d71ae92e1cac6
Jose Rosales <josecar25@gmail.com>
2022-04-04 15:18:00 +0200

    Making Conv1d compatible with Pytorch format via ONNX (scaling up the filter an input from NXCXL to NXCX1XL)

--- nn.go
+++ nn.go
@@ -2,9 +2,7 @@ package gorgonia
 
 import (
 	"fmt"
-	"time"
 
-	rng "github.com/leesper/go_rng"
 	"github.com/pkg/errors"
 	"gorgonia.org/gorgonia/internal/encoding"
 	"gorgonia.org/tensor"
@@ -67,11 +65,44 @@ func BinaryXent(output, target *Node) (retVal *Node, err error) {
 // It uses randomly zeroes out a *Tensor with a probability drawn from
 // a uniform distribution
 func Dropout(x *Node, dropProb float64) (retVal *Node, err error) {
-	rand := rng.NewUniformGenerator(time.Now().UnixNano())
+	return dropout(x, dropProb, UniformRandomNode)
+}
 
-	op := newDropoutOp(dropProb, func() float64 { return rand.Float64Range(0, 1) })
+type dropoutRandFn func(g *ExprGraph, dt tensor.Dtype, low, high float64, shape ...int) *Node
 
-	return ApplyOp(op, x)
+func dropout(x *Node, dropProb float64, randFn dropoutRandFn) (retVal *Node, err error) {
+	if dropProb == 0.0 {
+		return x, nil
+	}
+	keepProb := 1.0 - dropProb
+
+	var dt tensor.Dtype
+	if dt, err = dtypeOf(x.t); err != nil {
+		return nil, errors.Wrap(err, dtypeOfFail)
+	}
+
+	var pr Value
+	switch dt {
+	case Float64:
+		pr, _ = anyToScalar(keepProb)
+	case Float32:
+		pr, _ = anyToScalar(float32(keepProb))
+	default:
+		return nil, errors.Errorf(nyiTypeFail, "Dropout()", dt)
+	}
+
+	p := NewConstant(pr)
+
+	m := randFn(x.g, dt, 0, 1, x.shape...)
+	if retVal, err = Lt(m, p, true); err != nil {
+		return nil, errors.Wrap(err, "Greater Than failed")
+	}
+
+	if retVal, err = HadamardProd(x, retVal); err != nil {
+		return nil, errors.Wrap(err, mulFail)
+	}
+
+	return HadamardDiv(retVal, p)
 }
 
 // LeakyRelu returns a node whose underlying value is:
@@ -163,37 +194,42 @@ func Rectify(x *Node) (retVal *Node, err error) {
 // Im2Col converts a BCHW image block to columns. The kernel, pad and stride parameter must be shape of size 2, no more no less
 // This poor naming scheme clearly comes from matlab
 func Im2Col(n *Node, kernel, pad, stride, dilation tensor.Shape) (retVal *Node, err error) {
-	if kernel.Dims() != 2 {
-		return nil, errors.Errorf("kernel shape is supposed to have a dim of 2")
-	}
-	if pad.Dims() != 2 {
-		return nil, errors.Errorf("pad is supposed to have a dim of 2")
-	}
-	if stride.Dims() != 2 {
-		return nil, errors.Errorf("strides is supposed to have a dim of 2")
-	}
-	if dilation.Dims() != 2 {
-		return nil, errors.Errorf("dilation is supposed to have a dim of 2")
-	}
+        fmt.Println("JOSE3")
+        fmt.Println(kernel.Dims())
+        fmt.Println(kernel)
+        fmt.Println(dilation)
+        if kernel.Dims() != 2 {
+                return nil, errors.Errorf("kernel shape is supposed to have a dim of 2")
+        }
 
-	if kernel[0] <= 0 || kernel[1] <= 0 {
-		return nil, errors.Errorf("cannot have negative or 0 in kernel shape")
-	}
+        if pad.Dims() != 2 {
+                return nil, errors.Errorf("pad is supposed to have a dim of 2")
+        }
+        if stride.Dims() != 2 {
+                return nil, errors.Errorf("strides is supposed to have a dim of 2")
+        }
+        if dilation.Dims() != 2 {
+                return nil, errors.Errorf("dilation is supposed to have a dim of 2")
+        }
 
-	if stride[0] <= 0 || stride[1] <= 0 {
-		return nil, errors.Errorf("cannot have negative or 0 in stride: %v", stride)
-	}
+        if kernel[0] <= 0 || kernel[1] <= 0 {
+                return nil, errors.Errorf("cannot have negative or 0 in kernel shape")
+        }
 
-	if pad[0] < 0 || pad[1] < 0 {
-		return nil, errors.Errorf("cannot have negative padding")
-	}
+        if stride[0] <= 0 || stride[1] <= 0 {
+                return nil, errors.Errorf("cannot have negative or 0 in stride: %v", stride)
+        }
 
-	if dilation[0] <= 0 || dilation[1] <= 0 {
-		return nil, errors.Errorf("cannot have negative or 0 in dilation. %v", dilation)
-	}
+        if pad[0] < 0 || pad[1] < 0 {
+                return nil, errors.Errorf("cannot have negative padding")
+        }
 
-	op := makeIm2ColOp(kernel[0], kernel[1], pad[0], pad[1], stride[0], stride[1], dilation[0], dilation[1])
-	return ApplyOp(op, n)
+        if dilation[0] <= 0 || dilation[1] <= 0 {
+                return nil, errors.Errorf("cannot have negative or 0 in dilation. %v", dilation)
+        }
+
+        op := makeIm2ColOp(kernel[0], kernel[1], pad[0], pad[1], stride[0], stride[1], dilation[0], dilation[1])
+        return ApplyOp(op, n)
 }
 
 // Conv2d is a simple 2D convolution, to be used for CPU computation only.
@@ -207,99 +243,121 @@ func Im2Col(n *Node, kernel, pad, stride, dilation tensor.Shape) (retVal *Node, 
 // - stride: len(stride) == 2, example: []int{1, 1}
 // - dilation: len(dilation) == 2, defaults to []int{1, 1} if nil is passed
 func Conv2d(im, filter *Node, kernelShape tensor.Shape, pad, stride, dilation []int) (retVal *Node, err error) {
-	group := encoding.NewGroup("Convolution")
-	// niceness for defaults
-	if pad == nil {
-		pad = []int{0, 0}
-	}
-	if dilation == nil {
-		dilation = []int{1, 1}
-	}
+        group := encoding.NewGroup("Convolution")
+        // niceness for defaults
+        fmt.Println("BDBDBDBDDBDB")
+        fmt.Println(pad)
 
-	if im.Shape().Dims() != 4 {
-		return nil, fmt.Errorf("im should have 4 dims, got %v dims", im.Shape().Dims())
-	}
+        fmt.Println(dilation)
+        fmt.Println(stride)
+        if pad == nil {
+                pad = []int{0, 0}
+        }
+        if dilation == nil {
+                dilation = []int{1, 1}
+        }
+        if im.Shape().Dims() == 3 {
+                im, _ = Reshape(im, tensor.Shape{im.Shape()[0], im.Shape()[1], 1, im.Shape()[2]})
+                //return nil, fmt.Errorf("im should have 4 dims, got %v dims", im.Shape().Dims())
+        }
 
-	if filter.Shape().Dims() != 4 {
-		return nil, fmt.Errorf("filter should have 4 dims, got %v dims", filter.Shape().Dims())
-	}
+        if filter.Shape().Dims() == 3 {
+                filter, _ = Reshape(filter, tensor.Shape{filter.Shape()[0], filter.Shape()[2], 1, filter.Shape()[1]})
+//              return nil, fmt.Errorf("filter should have 4 dims, got %v dims", filter.Shape().Dims())
+        }
 
-	// checks
-	for _, s := range stride {
-		if s <= 0 {
-			return nil, errors.Errorf("Cannot use strides of less than or equal 0: %v", stride)
-		}
-	}
 
-	for _, p := range pad {
-		if p < 0 {
-			return nil, errors.Errorf("Cannot use padding of less than 0: %v", pad)
-		}
-	}
 
-	for _, d := range dilation {
-		if d <= 0 {
-			return nil, errors.Errorf("Cannot use dilation less than or eq 0 %v", dilation)
-		}
-	}
+        // checks
+        for _, s := range stride {
+                if s <= 0 {
+                        return nil, errors.Errorf("Cannot use strides of less than or equal 0: %v", stride)
+                }
+        }
 
-	var colIm *Node
-	if colIm, err = Im2Col(im, kernelShape, pad, stride, dilation); err != nil {
-		return nil, fmt.Errorf("Im2Col to failed: %w", err)
-	}
-	colIm.groups = colIm.groups.Upsert(group)
+        for _, p := range pad {
+                if p < 0 {
+                        return nil, errors.Errorf("Cannot use padding of less than 0: %v", pad)
+                }
+        }
 
-	layer := filter.Shape()[0]
-	kernel := filter.Shape()[1]
-	row := filter.Shape()[2]
-	col := filter.Shape()[3]
+        for _, d := range dilation {
+                if d <= 0 {
+                        return nil, errors.Errorf("Cannot use dilation less than or eq 0 %v", dilation)
+                }
+        }
 
-	if colIm.Shape()[3] != kernel*row*col {
-		return nil, fmt.Errorf("%d (kernel) * %d (width) * %d (height) must be %d, got %d", kernel, row, col, colIm.Shape()[3], kernel*row*col)
-	}
+        var colIm *Node
+        fmt.Println("JOSE5")
+        fmt.Println(kernelShape)
+        kernelShape = tensor.Shape{1,kernelShape[0]}
+        fmt.Println(kernelShape)
+        fmt.Println(dilation)
+        dilation = []int{1, dilation[0]}
+        fmt.Println(dilation)
+        fmt.Println(filter)
+        pad = []int{0, pad[0]}
+        fmt.Println("TSTSTSTSTSTS")
+        fmt.Println(pad)
+        if colIm, err = Im2Col(im, kernelShape, pad, stride, dilation); err != nil {
+                return nil, fmt.Errorf("Im2Col to failed: %w", err)
+        }
+        colIm.groups = colIm.groups.Upsert(group)
 
-	var flattened *Node
-	if flattened, err = Reshape(filter, tensor.Shape{layer, kernel * row * col}); err != nil {
-		return nil, fmt.Errorf("reshaping filter from %v to (%v, %v * %v * %v) failed: %w", filter.Shape(), layer, kernel, row, col, err)
-	}
-	flattened.groups = flattened.groups.Upsert(group)
+        layer := filter.Shape()[0]
+        kernel := filter.Shape()[1]
+        row := filter.Shape()[2]
+        col := filter.Shape()[3]
+                fmt.Println("JOSE6")
+                fmt.Println(colIm.Shape())
+        if colIm.Shape()[3] != kernel*row*col {
+                //fmt.Println("JOSE6")
+                //fmt.Println(colIm.Shape())
+                return nil, fmt.Errorf("%d (kernel) * %d (width) * %d (height) must be %d, got %d", kernel, row, col, colIm.Shape()[3], kernel*row*col)
+        }
 
-	// extract patch
-	batch := colIm.Shape()[0]
-	m := colIm.Shape()[1]
-	n := colIm.Shape()[2]
-	z := colIm.Shape()[3]
+        var flattened *Node
+        if flattened, err = Reshape(filter, tensor.Shape{layer, kernel * row * col}); err != nil {
+                return nil, fmt.Errorf("reshaping filter from %v to (%v, %v * %v * %v) failed: %w", filter.Shape(), layer, kernel, row, col, err)
+        }
+        flattened.groups = flattened.groups.Upsert(group)
 
-	var patch, colImLayer *Node
-	if patch, err = Reshape(colIm, tensor.Shape{batch * m * n, z}); err != nil {
-		return nil, fmt.Errorf("reshaping colIm from %v to (%v * %v * %v * %v) failed: %w", colIm.Shape(), batch, m, n, z, err)
-	}
-	patch.groups = patch.groups.Upsert(group)
+        // extract patch
+        batch := colIm.Shape()[0]
+        m := colIm.Shape()[1]
+        n := colIm.Shape()[2]
+        z := colIm.Shape()[3]
 
-	op := linAlgBinOp{
-		āBinaryOperator: matMulOperator,
-		transA:          false,
-		transB:          true,
-	}
+        var patch, colImLayer *Node
+        if patch, err = Reshape(colIm, tensor.Shape{batch * m * n, z}); err != nil {
+                return nil, fmt.Errorf("reshaping colIm from %v to (%v * %v * %v * %v) failed: %w", colIm.Shape(), batch, m, n, z, err)
+        }
+        patch.groups = patch.groups.Upsert(group)
 
-	if colImLayer, err = ApplyOp(op, patch, flattened); err != nil {
-		return nil, fmt.Errorf("failed to apply op: %w", err)
-	}
-	colImLayer.groups = colImLayer.groups.Upsert(group)
+        op := linAlgBinOp{
+                āBinaryOperator: matMulOperator,
+                transA:          false,
+                transB:          true,
+        }
 
-	// now reshape and transpose the values back into the original order
-	var res *Node
-	if res, err = Reshape(colImLayer, tensor.Shape{batch, m, n, layer}); err != nil {
-		return nil, fmt.Errorf("failed to reshape %v to (%v, %v, %v, %v): %w", colImLayer.Shape(), batch, m, n, layer, err)
-	}
-	res.groups = res.groups.Upsert(group)
-	ret, err := Transpose(res, 0, 3, 1, 2)
-	if err != nil {
-		return nil, fmt.Errorf("transpose %v failed: %w", res.Shape(), err)
-	}
+        if colImLayer, err = ApplyOp(op, patch, flattened); err != nil {
+                return nil, fmt.Errorf("failed to apply op: %w", err)
+        }
+        colImLayer.groups = colImLayer.groups.Upsert(group)
 
-	ret.groups = ret.groups.Upsert(group)
-	return ret, nil
+        // now reshape and transpose the values back into the original order
+        var res *Node
+        if res, err = Reshape(colImLayer, tensor.Shape{batch, m, n, layer}); err != nil {
+                return nil, fmt.Errorf("failed to reshape %v to (%v, %v, %v, %v): %w", colImLayer.Shape(), batch, m, n, layer, err)
+        }
+        res.groups = res.groups.Upsert(group)
+        ret, err := Transpose(res, 0, 3, 1, 2)
+        if err != nil {
+                return nil, fmt.Errorf("transpose %v failed: %w", res.Shape(), err)
+        }
+
+        ret.groups = ret.groups.Upsert(group)
+        return ret, nil
 }
 
 // Conv1d is a 1D convlution. It relies on Conv2D
@@ -320,6 +378,8 @@ func Conv1d(in, filter *Node, kernel, pad, stride, dilation int) (*Node, error) 
 func MaxPool2D(x *Node, kernel tensor.Shape, pad, stride []int) (*Node, error) {
 	group := encoding.NewGroup("Maxpool")
 	xShape := x.Shape()
+	h, w := xShape[2], xShape[3]
+	kh, kw := kernel[0], kernel[1]
 
 	// check shape
 	if xShape.Dims() != 4 {
@@ -341,9 +401,6 @@ func MaxPool2D(x *Node, kernel tensor.Shape, pad, stride []int) (*Node, error) {
 			return nil, errors.Errorf("Cannot use padding of less than 0: %v", pad)
 		}
 	}
-
-	h, w := xShape[2], xShape[3]
-	kh, kw := kernel[0], kernel[1]
 
 	padNorth := pad[0]
 	padWest := pad[1]
@@ -386,15 +443,56 @@ func BatchNorm(x, scale, bias *Node, momentum, epsilon float64) (retVal, γ, β 
 	if err != nil {
 		return nil, nil, nil, nil, err
 	}
-
+	batches := x.Shape()[0]
 	channels := x.Shape()[1]
+	spatialDim := x.Shape().TotalSize() / (channels * batches)
 
 	mean := tensor.New(tensor.Of(dt), tensor.WithShape(channels))
 	variance := tensor.New(tensor.Of(dt), tensor.WithShape(channels))
+	ma := tensor.New(tensor.Of(dt), tensor.WithShape(1))
 
-	saveMean := tensor.New(tensor.Of(dt), tensor.WithShape(channels))
-	saveVar := tensor.New(tensor.Of(dt), tensor.WithShape(channels))
+	meanTmp := tensor.New(tensor.Of(dt), tensor.WithShape(channels))
+	varianceTmp := tensor.New(tensor.Of(dt), tensor.WithShape(channels))
+	tmp := tensor.New(tensor.Of(dt), tensor.WithShape(x.Shape().Clone()...))
+	xNorm := tensor.New(tensor.Of(dt), tensor.WithShape(x.Shape().Clone()...))
+	batchSumMultiplier := tensor.New(tensor.Of(dt), tensor.WithShape(batches))
 
+	var uno interface{}
+	switch dt {
+	case Float64:
+		uno = float64(1)
+	case Float32:
+		uno = float32(1)
+	}
+	spatialSumMultiplier := tensor.New(tensor.Of(dt), tensor.WithShape(spatialDim))
+	if err = spatialSumMultiplier.Memset(uno); err != nil {
+		return nil, nil, nil, nil, err
+	}
+
+	numByChans := tensor.New(tensor.Of(dt), tensor.WithShape(channels*batches))
+	if err = batchSumMultiplier.Memset(uno); err != nil {
+		return nil, nil, nil, nil, err
+	}
+
+	op = &BatchNormOp{
+		momentum: momentum,
+		epsilon:  epsilon,
+
+		mean:     mean,
+		variance: variance,
+		ma:       ma,
+
+		meanTmp:              meanTmp,
+		varianceTmp:          varianceTmp,
+		tmpSpace:             tmp,
+		xNorm:                xNorm,
+		batchSumMultiplier:   batchSumMultiplier,
+		numByChans:           numByChans,
+		spatialSumMultiplier: spatialSumMultiplier,
+
+		training: true,
+		dims:     x.Dims(),
+	}
 	g := x.Graph()
 	dims := x.Shape().Dims()
 
@@ -405,29 +503,12 @@ func BatchNorm(x, scale, bias *Node, momentum, epsilon float64) (retVal, γ, β 
 		bias = NewTensor(g, dt, dims, WithShape(x.Shape().Clone()...), WithName(x.Name()+"_β"), WithInit(GlorotN(1.0)))
 	}
 
-	op = &BatchNormOp{
-		momentum: momentum,
-		epsilon:  epsilon,
-
-		runningMean:     mean,
-		runningVariance: variance,
-
-		saveMean:     saveMean,
-		saveVariance: saveVar,
-
-		training: true,
-		dims:     x.Dims(),
-	}
-
 	if retVal, err = ApplyOp(op, x); err != nil {
 		return nil, nil, nil, nil, err
 	}
-
-	retVal, err = Auto(BroadcastHadamardProd, scale, retVal)
-	if err != nil {
+	if retVal, err = Auto(BroadcastHadamardProd, scale, retVal); err != nil {
 		return nil, nil, nil, nil, err
 	}
-
 	retVal, err = Auto(BroadcastAdd, retVal, bias)
 
 	return retVal, scale, bias, op, err
